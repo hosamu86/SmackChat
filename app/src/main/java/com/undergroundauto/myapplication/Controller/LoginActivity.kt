712527d2b493f2c2f02a5d61075a0f6fe974861a
hosam <hosamww2000@hotmail.com>
2018-08-04 10:01:26 +0300

    almost Connecting APi

---
+++ app/src/main/java/com/undergroundauto/myapplication/Controller/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.undergroundauto.myapplication.Controller
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.undergroundauto.myapplication.R
+
+class LoginActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+    fun loginLoginBClicked(view: View){
+
+    }
+    fun registerBClicked(view: View){
+   val registerIntent = Intent(this, RegisterActivity::class.java)
+        startActivity(registerIntent)
+    }
+
+}
