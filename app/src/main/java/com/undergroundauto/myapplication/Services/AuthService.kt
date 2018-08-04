e0fe3b03af0c1e0869cbe852a4802e921dc5551b
hosam <hosamww2000@hotmail.com>
2018-08-04 09:56:59 +0300

    almost Connecting APi

---
+++ app/src/main/java/com/undergroundauto/myapplication/Services/AuthService.kt
@@ -0,0 +1,5 @@
+package com.undergroundauto.myapplication.Services
+
+object AuthService {
+
+}
---
+++ app/src/main/java/com/undergroundauto/myapplication/Utilities/Constants.kt
@@ -0,0 +1,4 @@
+package com.undergroundauto.myapplication.Utilities
+
+const val BASE_URL = "https://huzzza.herokuapp.com/"
+const val URL_REGISTER = "${BASE_URL}account/register"
